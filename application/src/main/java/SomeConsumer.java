804ac065781b219d53363f7af16bf1b715f56187
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2022-05-15 11:03:25 +0300

    Add more gradle build configuration feature and use a different lib in application.
    The aws core lib used in the library project is 1.12.205.

--- application/src/main/java/SomeConsumer.java
+++ application/src/main/java/SomeConsumer.java
@@ -1,6 +1,24 @@
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+
 public class SomeConsumer {
     public static void main(String[] args) {
         AwsConnectionsManager manager = new AwsConnectionsManager();
         System.out.println("Access key is: " + manager.getCreds().getCredentials().getAWSAccessKeyId());
+
+        //using a different version of aws core
+        //(lib was compiled with a different version of aws core sdk)
+        AWSCredentialsProvider someCredsprovider = new AWSCredentialsProvider() {
+            @Override
+            public AWSCredentials getCredentials() {
+                return null;
+            }
+
+            @Override
+            public void refresh() {
+
+            }
+        };
+        System.out.println("Creds provider from the core: " + someCredsprovider.getCredentials());
     }
 }
